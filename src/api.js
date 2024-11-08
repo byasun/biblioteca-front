fd0eb413279f1a468828ba787a01a064401c643d
byasun <annaclarag6@gmail.com>
2024-11-08 13:37:41 -0300

    correção api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,18 @@
+import axios from 'axios';  // Certifique-se de que o axios está importado
+
 const apiUrl = process.env.REACT_APP_API_URL;
 
-axios.get(`${apiUrl}/api/rota`)
+const api = axios.create({
+  baseURL: apiUrl,
+});
+
+api.get('/api/rota')
   .then(response => {
     console.log(response.data);
   })
   .catch(error => {
     console.error('Erro ao fazer requisição ao backend', error);
   });
+
+// Exportando o axios configurado
+export default api;
