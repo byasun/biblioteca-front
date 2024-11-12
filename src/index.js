451aa8b5e9021ac4dfe076d5d1461ca1e9da51e5
byasun <annaclarag6@gmail.com>
2024-11-12 16:26:24 -0300

    teste Auth0

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider, useDispatch } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';  // Mantenha o BrowserRouter aqui
-import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
-import store from './redux/store'; // Importa a loja Redux
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import store from './redux/store';
 import App from './App';
 import './styles/GlobalStyles';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
-const Auth0ProviderWithRedux = ({ children }) => {
-  const dispatch = useDispatch();
-  const { isAuthenticated, user } = useAuth0();
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      dispatch({ type: 'LOGIN_SUCCESS', payload: user });
-    }
-  }, [isAuthenticated, user, dispatch]);
-
-  return children;
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -32,10 +19,8 @@ root.render(
       redirectUri={window.location.origin}
     >
       <Provider store={store}>
-        <Router> {/* Coloque o BrowserRouter apenas aqui */}
-          <Auth0ProviderWithRedux>
-            <App />
-          </Auth0ProviderWithRedux>
+        <Router>
+          <App />
         </Router>
       </Provider>
     </Auth0Provider>
