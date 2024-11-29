18043f3679ca8a641d02b5d32c2c30f6b9a3ecc4
byasun <annaclarag6@gmail.com>
2024-11-29 16:04:41 -0300

    Merge branch 'correção-de-api' of https://github.com/byasun/biblioteca-front

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,6 +1,5 @@
 export const appConfig = {
-    appName: 'Biblioteca',
-    defaultLanguage: 'pt-BR',
-    supportEmail: 'suporte@biblioteca.com',
-  };
-  
+  appName: 'Biblioteca',
+  defaultLanguage: 'pt-BR',
+  supportEmail: 'suporte@biblioteca.com',
+};
