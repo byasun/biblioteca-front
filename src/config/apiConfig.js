fb1cb699c8a9208e10afc3fc3a8ee7786644e6e4
Anna Clara Gomes <102702295+byasun@users.noreply.github.com>
2024-12-04 17:31:35 +0000

    teste urls

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 // src/config/apiConfig.js
 export const apiConfig = {
-    baseURL: process.env.REACT_APP_API_BASE_URL || '/api', // Define a base URL como variável de ambiente ou padrão "/api"
+    baseURL: process.env.REACT_APP_API_URL , // Define a base URL como variável de ambiente ou padrão "/api"
   };
   
