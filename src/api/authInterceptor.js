865ed8d1bff1586c43c783c41886e8454fcbb14d
Anna Clara Gomes <102702295+byasun@users.noreply.github.com>
2024-12-10 16:54:02 +0000

    teste 2

--- src/api/authInterceptor.js
+++ src/api/authInterceptor.js
@@ -2,8 +2,6 @@ export const authInterceptor = (config) => {
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
-  } else {
-    console.warn('Token de autenticação não encontrado.');
   }
   return config;
 };
