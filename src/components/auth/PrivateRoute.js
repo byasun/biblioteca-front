d8ce187cba67076b0d1e1e4ecee8606c865d59b0
Anna Clara Gomes <102702295+byasun@users.noreply.github.com>
2024-12-13 12:55:10 +0000

    correção PrivateRoutes

--- src/components/auth/PrivateRoute.js
+++ src/components/auth/PrivateRoute.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { ROUTES } from '../../routes/paths';
 
 const PrivateRoute = ({ children }) => {
   const isAuthenticated = useSelector((state) => state.usuario.isAuthenticated);
