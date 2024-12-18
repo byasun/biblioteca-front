807f825683517f71ec5faf742202f45c4ee1df89
byasun <annaclarag6@gmail.com>
2024-12-18 10:44:36 -0300

    errata logout 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,9 @@ import Navbar from '../styles/layout/Navbar';
 import Footer from '../styles/layout/Footer';
 import Carousel from '../components/common/Carrossel';
 
+import { useDispatch } from 'react-redux';
+import { logout } from '../redux/usuarios/usuarioSlice';
+
 const Home = () => (
   <div className="page-container">
     <Navbar />
@@ -10,7 +13,7 @@ const Home = () => (
       <h1>Bem-vindo à Biblioteca da REGAP</h1>
       <p>Estamos com uma nova missão, a de compartilhar conhecimento e histórias com todos e para isso trazemos as estantes viajantes.</p>
       <Carousel />
-      <button onClick={handleLogout}>Sair</button>
+      <button onClick={logout}>Sair</button>
     </div>
     <Footer />
   </div>
